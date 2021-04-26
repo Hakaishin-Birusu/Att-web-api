a9cea2de7e3ced393badb5efeadb7b941bd4fc57
sagar chaurasia <chaurasiasagar01@gmail.com>
2021-04-26 07:20:41 +0530

    api criteria added

--- routes/stake.js
+++ routes/stake.js
@@ -6,16 +6,29 @@ router.get("/stats", function (req, res) {
   stakeService.Stats(req, res);
 });
 
+// 1. total staked
+// 2. total reward
+// 3. reward resets timer
+// 4. apy
+// 5. reward multiplier
+
 router.get("/stakeinfo/:user", function (req, res) {
   stakeService.StakeInfo(req, res);
 });
+
+// may be removed
 
 router.get("/userstake/:user", function (req, res) {
   stakeService.UserStake(req, res);
 });
 
+// 1. user balance
+
 router.get("/userunstake/:user", function (req, res) {
   stakeService.UserUnstake(req, res);
 });
+
+// 1. user staked balance
+// 2. expected return
 
 module.exports = router;
--- routes/xAtt.js
+++ routes/xAtt.js
@@ -5,17 +5,31 @@ const xAttService = require("../services/xAttHandler");
 router.get("/stats", function (req, res) {
   xAttService.Stats(req, res);
 });
+// 1. total staked
+// 2. total reward
+// 3. reward resets timer
+// 4. apy
+// 5. reward multiplier
+// 6. xAtt price
 
 router.get("/stakeinfo/:user", function (req, res) {
   xAttService.StakeInfo(req, res);
 });
 
+// may be removed
+
 router.get("/userstake/:user", function (req, res) {
   xAttService.UserStake(req, res);
 });
 
+// 1. user balance
+// 2. current position
+
 router.get("/userunstake/:user", function (req, res) {
   xAttService.UserUnstake(req, res);
 });
+
+// 1. user staked balance
+// 2. expected return
 
 module.exports = router;
